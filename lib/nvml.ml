108466d9cfb70d14e2f503954fd14ebee6331fa8
John Else <john.else@citrix.com>
2013-09-23 01:18:17 +0100

    More work on Device module
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/nvml.ml
+++ lib/nvml.ml
@@ -14,6 +14,17 @@ let init = foreign ~from:libnvml "nvmlInit" (void @-> returning int)
 let shutdown = foreign ~from:libnvml "nvmlShutdown" (void @-> returning int)
 
 module Device = struct
+	type t
+	let t : t structure typ = structure "nvmlDevice_t"
+	let handle = t *:* uint64_t
+	let () = seal t
+
 	let get_count = foreign ~from:libnvml "nvmlDeviceGetCount"
 		(ptr uint @-> returning int)
+
+	let get_handle_by_index = foreign ~from:libnvml "nvmlDeviceGetHandleByIndex"
+		(uint @-> ptr t @-> returning int)
+
+	let get_fan_speed = foreign ~from:libnvml "nvmlDeviceGetFanSpeed"
+		(t @-> ptr uint @-> returning int)
 end
