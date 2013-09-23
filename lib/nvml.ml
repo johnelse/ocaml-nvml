f88d7784049b5385ee506e574b8a464107cbbf1b
John Else <john.else@citrix.com>
2013-09-23 16:58:30 +0100

    Define the foreign functions in the top level of the module
    This means:
    
    * The foreign functions only get set up once.
    * Any problems with the foreign functions are thrown up when the module
      is opened, rather than when the native functions are called.
    
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/nvml.ml
+++ lib/nvml.ml
@@ -63,21 +63,15 @@ let check_error f =
 	| 15 -> fail GPU_is_lost
 	| _ -> fail Unknown
 
-let string_of_error ~error =
-	let string_of_error' =
-		foreign ~from:libnvml "nvmlErrorString" (int @-> returning string)
-	in
-	string_of_error' (int_of_error error)
+let string_of_error' =
+	foreign ~from:libnvml "nvmlErrorString" (int @-> returning string)
+let string_of_error ~error = string_of_error' (int_of_error error)
 
-let init () =
-	let init' = foreign ~from:libnvml "nvmlInit" (void @-> returning int) in
-	check_error init'
+let init' = foreign ~from:libnvml "nvmlInit" (void @-> returning int)
+let init () = check_error init'
 
-let shutdown () =
-	let shutdown' =
-		foreign ~from:libnvml "nvmlShutdown" (void @-> returning int)
-	in
-	check_error shutdown'
+let shutdown' = foreign ~from:libnvml "nvmlShutdown" (void @-> returning int)
+let shutdown () = check_error shutdown'
 
 module Util = struct
 	let string_of_char_array array =
@@ -100,57 +94,51 @@ module Device = struct
 	let handle = t *:* uint64_t
 	let () = seal t
 
+	let get_count' =
+		foreign ~from:libnvml "nvmlDeviceGetCount" (ptr uint @-> returning int)
 	let get_count () =
-		let get_count' =
-			foreign ~from:libnvml "nvmlDeviceGetCount" (ptr uint @-> returning int)
-		in
 		let count_ptr = allocate uint (UInt.of_int 0) in
 		check_error (fun () -> get_count' count_ptr);
 		!@ count_ptr
 
+	let get_fan_speed' =
+		foreign ~from:libnvml "nvmlDeviceGetFanSpeed"
+			(t @-> ptr uint @-> returning int)
 	let get_fan_speed ~device =
-		let get_fan_speed' =
-			foreign ~from:libnvml "nvmlDeviceGetFanSpeed"
-				(t @-> ptr uint @-> returning int)
-		in
 		let fan_speed_ptr = allocate uint (UInt.of_int 0) in
 		check_error (fun () -> get_fan_speed' device fan_speed_ptr);
 		!@ fan_speed_ptr
 
+	let get_handle_by_index' =
+		foreign ~from:libnvml "nvmlDeviceGetHandleByIndex"
+			(uint @-> ptr t @-> returning int)
 	let get_handle_by_index ~index =
-		let get_handle_by_index' =
-			foreign ~from:libnvml "nvmlDeviceGetHandleByIndex"
-				(uint @-> ptr t @-> returning int)
-		in
 		let device = make t in
 		check_error (fun () -> get_handle_by_index' index (addr device));
 		device
 
+	let get_name' =
+		foreign ~from:libnvml "nvmlDeviceGetName"
+			(t @-> ptr char @-> uint @-> returning int)
 	let get_name ~device =
-		let get_name' =
-			foreign ~from:libnvml "nvmlDeviceGetName"
-				(t @-> ptr char @-> uint @-> returning int)
-		in
 		let length = 64 in
 		let name_array = Array.make char length in
 		let name_ptr = Array.start name_array in
 		check_error (fun () -> get_name' device name_ptr (UInt.of_int length));
 		Util.string_of_char_array name_array
 
+	let get_power_usage' =
+		foreign ~from:libnvml "nvmlDeviceGetPowerUsage"
+			(t @-> ptr uint @-> returning int)
 	let get_power_usage ~device =
-		let get_power_usage' =
-			foreign ~from:libnvml "nvmlDeviceGetPowerUsage"
-				(t @-> ptr uint @-> returning int)
-		in
 		let power_usage_ptr = allocate uint (UInt.of_int 0) in
 		check_error (fun () -> get_power_usage' device power_usage_ptr);
 		!@ power_usage_ptr
 
+	let on_same_board' =
+		foreign ~from:libnvml "nvmlDeviceOnSameBoard"
+			(t @-> t @-> ptr int @-> returning int)
 	let on_same_board ~device1 ~device2 =
-		let on_same_board' =
-			foreign ~from:libnvml "nvmlDeviceOnSameBoard"
-				(t @-> t @-> ptr int @-> returning int)
-		in
 		let on_same_board_ptr = allocate int 0 in
 		check_error (fun () -> on_same_board' device1 device2 on_same_board_ptr);
 		(!@ on_same_board_ptr) <> 0
