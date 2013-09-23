2201f5b761c01c6609aaf8e6650a4e2841395a7f
John Else <john.else@citrix.com>
2013-09-23 13:15:19 +0100

    Initial commit of nvml.mli
    Signed-off-by: John Else <john.else@citrix.com>

---
+++ lib/nvml.mli
@@ -0,0 +1,38 @@
+open Ctypes
+
+type error =
+	| Uninitialized
+	| Invalid_argument
+	| Not_supported
+	| No_permission
+	| Already_initialized
+	| Not_found
+	| Insufficient_size
+	| Insufficient_power
+	| Driver_not_loaded
+	| Timeout
+	| IRQ_issue
+	| Library_not_found
+	| Function_not_found
+	| Corrupted_infoROM
+	| GPU_is_lost
+	| Unknown
+
+exception Error of error
+
+val string_of_error : error:error -> string
+
+val init : unit -> unit
+
+val shutdown : unit -> unit
+
+module Device : sig
+	type t
+	val t : t structure typ
+
+	val get_count : unit -> Unsigned.uint
+
+	val get_handle_by_index : index:Unsigned.uint -> t Ctypes.structure
+
+	val get_fan_speed : device:t Ctypes.structure -> Unsigned.uint
+end
