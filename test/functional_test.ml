9e57d6235c5327ead6be6e37bd314efc27e3d394
John Else <john.else@citrix.com>
2013-09-23 18:51:39 +0100

    Add Device.get_memory_info to the test
    Signed-off-by: John Else <john.else@citrix.com>

--- test/functional_test.ml
+++ test/functional_test.ml
@@ -9,10 +9,17 @@ let () =
 		let name = Nvml.Device.get_name ~device in
 		let uuid = Nvml.Device.get_uuid ~device in
 		let fan_speed = Nvml.Device.get_fan_speed ~device in
+		let memory = Nvml.Device.get_memory_info ~device in
 		Printf.printf "--------------------\n";
 		Printf.printf "Device index = %d\n" index;
 		Printf.printf "Name = %s\n" name;
 		Printf.printf "UUID = %s\n" uuid;
-		Printf.printf "Fan speed = %d\n" (UInt.to_int fan_speed)
+		Printf.printf "Fan speed = %d\n" (UInt.to_int fan_speed);
+		Printf.printf "Memory total = %d\n"
+			(ULLong.to_int (Ctypes.getf memory Nvml.Memory.total));
+		Printf.printf "Memory free = %d\n"
+			(ULLong.to_int (Ctypes.getf memory Nvml.Memory.free));
+		Printf.printf "Memory used = %d\n"
+			(ULLong.to_int (Ctypes.getf memory Nvml.Memory.used))
 	done;
 	Nvml.shutdown ()
