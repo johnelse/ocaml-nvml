8cf4af5423d31008b08985ca74d8bfe9099ad9a4
John Else <john.else@citrix.com>
2013-09-25 11:37:36 +0100

    Add system queries to test
    Signed-off-by: John Else <john.else@citrix.com>

--- test/functional_test.ml
+++ test/functional_test.ml
@@ -1,7 +1,17 @@
 open Unsigned
 
+let separator () = Printf.printf "--------------------\n"
+
 let () =
 	Nvml.init ();
+	(* Query and print system information. *)
+	separator ();
+	let driver_version = Nvml.System.get_driver_version () in
+	let nvml_version = Nvml.System.get_nvml_version () in
+	Printf.printf "System driver version = %s\n" driver_version;
+	Printf.printf "NVML version = %s\n" nvml_version;
+	(* Query and print device information. *)
+	separator ();
 	let count = UInt.to_int (Nvml.Device.get_count ()) in
 	(match count with
 	| 1 -> Printf.printf "There is %d device installed.\n" count
@@ -12,7 +22,7 @@ let () =
 		let uuid = Nvml.Device.get_uuid ~device in
 		let fan_speed = Nvml.Device.get_fan_speed ~device in
 		let memory = Nvml.Device.get_memory_info ~device in
-		Printf.printf "--------------------\n";
+		separator ();
 		Printf.printf "Device index = %d\n" index;
 		Printf.printf "Name = %s\n" name;
 		Printf.printf "UUID = %s\n" uuid;
